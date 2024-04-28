1585c53c154f31fd5073c6d54af2f1d89d65b29f
Malsha-Weerasinghe <weerasinghemalsha4@gmail.com>
2024-04-28 21:01:55 +0530

    Created employee service interface

--- src/main/java/lk/ijse/shoeShop/service/EmployeeService.java
+++ src/main/java/lk/ijse/shoeShop/service/EmployeeService.java
@@ -1,4 +1,14 @@
 package lk.ijse.shoeShop.service;
 
-public class EmployeeService {
+import lk.ijse.shoeShop.dto.CustomerDTO;
+import lk.ijse.shoeShop.dto.EmployeeDTO;
+
+import java.util.List;
+
+public interface EmployeeService {
+    List<EmployeeDTO> getAllCustomers();
+    EmployeeDTO getEmployeeDetails(String employeeCode);
+    EmployeeDTO saveEmployee(EmployeeDTO employeeDTO);
+    void updateEmployee(String employeeCode, EmployeeDTO EmployeeDTO);
+    void deleteEmployee(String EmployeeCode);
 }
