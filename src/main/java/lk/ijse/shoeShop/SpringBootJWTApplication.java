22d0244ba3d07c7fb30632df3f7a2267d2fe77d7
Malsha-Weerasinghe <weerasinghemalsha4@gmail.com>
2024-05-22 21:38:04 +0530

    Updated user service interface

--- src/main/java/lk/ijse/shoeShop/SpringBootJWTApplication.java
+++ src/main/java/lk/ijse/shoeShop/SpringBootJWTApplication.java
@@ -4,20 +4,16 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
-import java.io.IOException;
 
 @SpringBootApplication
 public class SpringBootJWTApplication {
-
-    public SpringBootJWTApplication() throws IOException {
-    }
 
     public static void main(String[] args) {
         SpringApplication.run(SpringBootJWTApplication.class, args);
     }
 
     @Bean
-    public ModelMapper mapper(){
+    public ModelMapper modelMapper(){
         return new ModelMapper();
     }
 }
--- src/main/java/lk/ijse/shoeShop/service/UserService.java
+++ src/main/java/lk/ijse/shoeShop/service/UserService.java
@@ -1,19 +1,16 @@
-/*
 package lk.ijse.shoeShop.service;
 
 import lk.ijse.shoeShop.dto.UserDTO;
+import lk.ijse.shoeShop.util.Role;
 import org.springframework.security.core.userdetails.UserDetailsService;
 
 import java.util.List;
 
-public interface UserService extends UserDetailsService
-{
-
+public interface UserService {
     UserDetailsService userDetailService();
     List<UserDTO> getAllUser();
-    UserDTO getUserDetails(String email,String role);
+    UserDTO getUserDetails(String email, Role role);
     UserDTO saveUser(UserDTO userDTO);
     void updateUser(String email, UserDTO userDTO);
     void deleteUser(String email);
 }
-*/
